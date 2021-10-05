7436581f023cae72a6360c1797fb52517bf697b8
cenkAkay <cenkakay@rotateknik.com.tr>
2021-10-05 09:15:35 +0300

    template driven form assignment finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  @ViewChild("form", { static: false }) signupForm: NgForm;
 
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
